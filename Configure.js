d12ec0853d415501ce4e00022e99d92d915f11e3
Ivan Bogush <kemazode@mail.ru>
2023-01-19 11:04:42 +0300

    Add files via upload

--- Configure.js
+++ Configure.js
@@ -3,6 +3,30 @@ DT_UploadResources();
 options = ['Никто', ...Object.keys(DT_Units)];
 boxes = [];
 
+gameui = document.getElementById('gameui');
+ui = document.getElementById('ui');
+armyconf = document.getElementById('armyconf');
+
+function arrayToHexString(arr) {
+    let hexString = "";
+    for (let i = 0; i < arr.length; i++) {
+        let hexVal = Number(arr[i].value).toString(16);
+        if (hexVal.length < 2) {
+            hexVal = "0" + hexVal;
+        }
+        hexString += hexVal;
+    }
+    return hexString;
+}
+
+function hexStringToArray(hexString, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let hexVal = hexString.substring(i*2, i*2+2);
+        arr[i].value = parseInt(hexVal, 16);
+    }
+    return arr;
+}
+
 function createLine(name, label, units) {
     block = document.createElement('div');
     lhtml = document.createElement('label');
@@ -17,15 +41,15 @@ function createLine(name, label, units) {
 
         for (let i = 0; i < options.length; i++) {
             ohtml = document.createElement('option')
-            ohtml.setAttribute('value', options[i]);
+            ohtml.setAttribute('value', i);
             ohtml.innerText = options[i];
             box.appendChild(ohtml);
         }        
-
+        boxes.push(box);
         block.appendChild(box);
     }
 
-    document.body.prepend(block);
+    ui.prepend(block);
 }
 
 createLine('4', 'Арьергард:', DT_PlayerRear);
@@ -34,26 +58,18 @@ createLine('2', 'Форвард (вр.):', DT_EnemyForward);
 createLine('1', 'Арьергард (вр.):', DT_EnemyRear);
 
 document.getElementById('start').addEventListener('click', function() {
+    gameui.style.visibility = 'visible';
+    ui.style.visibility = 'hidden';
     DT_ClearArmies();
     DT_ClearElements();
     player_units = [];
     enemy_units = [];
 
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('3' + j)[0];
-        player_units.push(box.value);
+    for (let i = 0; i < 12; i++) {
+        player_units.push(options[boxes[i].value]);
     }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('4' + j)[0];
-        player_units.push(box.value);
-    }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('2' + j)[0];
-        enemy_units.push(box.value);
-    }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('1' + j)[0];
-        enemy_units.push(box.value);
+    for (let i = 12; i < 24; i++) {
+        enemy_units.push(options[boxes[i].value]);
     }
 
     console.log(player_units);
@@ -65,41 +81,34 @@ document.getElementById('start').addEventListener('click', function() {
 });
 
 document.getElementById('shuffle').addEventListener('click', function() {
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('3' + j)[0];
-        box.value = options[getRandomInt(options.length)];
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].value = getRandomInt(options.length - 1) + 1;
     }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('4' + j)[0];
-        box.value = options[getRandomInt(options.length)];
-    }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('2' + j)[0];
-        box.value = options[getRandomInt(options.length)];
-    }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('1' + j)[0];
-        box.value = options[getRandomInt(options.length)];
-    }
+    armyconf.value = arrayToHexString(boxes);
 });
 
 document.getElementById('reset').addEventListener('click', function() {
     DT_ClearElements();
     DT_ClearArmies();
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('3' + j)[0];
-        box.value = options[0];
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].value = 0;
     }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('4' + j)[0];
-        box.value = options[0];
-    }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('2' + j)[0];
-        box.value = options[0];
-    }
-    for (let j = 0; j < 6; j++) {
-        box = document.getElementsByName('1' + j)[0];
-        box.value = options[0];
-    }
+    armyconf.value = arrayToHexString(boxes);
+});
+
+document.getElementById('finish').addEventListener('click', function() {
+    gameui.style.visibility = 'hidden';
+    ui.style.visibility = 'visible';
+    DT_ClearElements();
+    DT_ClearArmies();
+});
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('change', (event) => { 
+        armyconf.value = arrayToHexString(boxes);
+    });
+}
+
+armyconf.addEventListener('change', (event) => { 
+    hexStringToArray(armyconf.value, boxes);
 });
